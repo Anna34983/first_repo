7d207b2750f6e1156ffb46022349cd7d23e40180
Anna <skiba_anna@o2.pl>
2024-12-14 09:30:45 +0100

    projekt3

--- powtorka2.py
+++ powtorka2.py
@@ -1,7 +1,18 @@
-print('Hello')
+# print('Hello')
 
-input()
+# input()
+
 age = int(input('Ile masz lat? '))
 
-print(f'Wiec masz {age} lat') # f oznacza że w nawiasie { masz zmienna
-print(f'Bedziesz dorosly za {18 - age} lat')
+# print(f'Wiec masz {age} lat') # f oznacza że w nawiasie { masz zmienna
+# print(f'Bedziesz dorosly za {18 - age} lat')
+
+while True:
+    if 0 < age < 18:
+        print(f'wiec masz {age} lat')
+        print(f'bedziesz dorosly za {18 - age} lat')
+        break
+    else:
+        print('zly wiek')
+        break
+print('Dalsza czesc programu')
