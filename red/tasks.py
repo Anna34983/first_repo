1c06c36dc3c221bef278f68c761181ccd86a3794
Anna <skiba_anna@o2.pl>
2025-01-12 10:20:10 +0100

    12012025_2

--- red/tasks.py
+++ red/tasks.py
@@ -1,6 +1,10 @@
 import requests
+from redis import Redis
 
 def fetch_weather_data(station_id):
+
+    redis_conn = Redis()
+
     try:
         print(f"Pobieranie danych dla stacji {station_id}...")
 
@@ -9,20 +13,22 @@ def fetch_weather_data(station_id):
         if response.status_code == 200:
             data = response.json()
             temp = float(data["temperature"])
-            
+
             if temp > 30:
-                print(f"wysoka temperatura {temp}'C na stacji {station_id}")
+                print(f"Wysoka temperatura {temp}'C na stacji {station_id}")
+
+                redis_conn.lpush(f"temps:{station_id}", temp)
             return data
 
         else:
             print(f"Błąd API, kod odpowiedzi: {response.status_code}")
             return None
     except requests.exceptions.ConnectionError:
-        print(f"Nie można polaczyc sie z API dla staci {station_id}")
+        print(f"Nie można połączyć się z API dla stacji {station_id}")
         return None
     except Exception as e:
-        print(f"Blad dla stacji {station_id} {e}")
+        print(f"Błąd dla stacji {station_id} {e}")
         return None
 
 if __name__ == "__main__":
-    fetch_weather_data("STACJA_TESTOWA")
+    print(fetch_weather_data("STACJA_TESTOWA"))
